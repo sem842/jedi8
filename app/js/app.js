10580159d67b6ccffbe9711015e1744e23bcba92
sem842 <gsem842@gmail.com>
2021-04-28 17:50:33 +0700

    Проведена работа над слайдером: слайды картинок и текста.

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,26 @@
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
+import { Swiper, Parallax, Mousewheel, Controller } from 'swiper'
+Swiper.use([ Parallax, Mousewheel, Controller ])
+
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	const swiperIMG = new Swiper('.slider-img', {
+		loop: false,
+		speed: 2400,
+		parallax: true
+	})
+
+	const swiperText = new Swiper('.slider-text', {
+		loop: false,
+		speed: 2400,
+		mousewheel: {
+			invert: false
+		}
+	})
+
+	swiperIMG.controller.control = swiperText
+	swiperText.controller.control = swiperIMG
 
 })
